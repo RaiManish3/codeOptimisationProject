ce350e2f0b82699602d7d3199036561f3972679d
RaiManish3 <manish.07.1996@gmail.com>
2017-06-08 02:49:13 +0530

    Thanks tenzin :: decorator highlighting works and color changed

--- checkthisfile/src/extension.ts
+++ checkthisfile/src/extension.ts
@@ -12,21 +12,20 @@ export function activate(context: vscode.ExtensionContext) {
     // This line of code will only be executed once when your extension is activated
     //console.log('Congratulations, your extension "checkthisfile" is now active!');
 
+    let decorationType = vscode.window.createTextEditorDecorationType(<vscode.DecorationRenderOptions>{
+            backgroundColor: 'rgba(200,10,10,0.5)'
+        })
+
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('extension.checkMe', () => {
         // The code you place here will be executed every time your command is executed
         let editor = vscode.window.activeTextEditor
-        let end_line = editor.document.lineCount;
-        let end_col = editor.document.lineAt(end_line-1).text.length;
-        // initially tried to replace existing Decorator with the new "transparent" one and 
-        // but what it does that it superimposes the one on the other rather than removing it
+        //let end_line = editor.document.lineCount;  //get the line count
+        //let end_col = editor.document.lineAt(end_line-1).text.length; // get the column count of last line
 
-        let decorationType = vscode.window.createTextEditorDecorationType({
-            backgroundColor: 'red'
-        })
-
+        decorationType.dispose();
         var PythonShell = require('python-shell');
         let python_check_script = 'python_scripts/test.py';
         let inFile = 'python_scripts/testing/check1.txt'
@@ -36,15 +35,17 @@ export function activate(context: vscode.ExtensionContext) {
                 input: require('fs').createReadStream(inFile)
             });
 
-            let i = 1;
-            let decs = [] 
+        let i = 1;
+        let decs = []
+        decorationType = vscode.window.createTextEditorDecorationType(<vscode.DecorationRenderOptions>{
+            backgroundColor: 'rgba(200,10,10,0.5)'
+        }) 
             lineReader.on('line', function (line) {
                 if(i%4==0){
-                    let line_split = line.split(",");
-                    //console.log('Line from file:', line_split);     
+                    let line_split = line.split(",");  
                     let pos1 = new vscode.Range(parseInt(line_split[0]),parseInt(line_split[1]),parseInt(line_split[2]),parseInt(line_split[3]));
-                    decs.push(pos1);      
-                    editor.setDecorations(decorationType,decs);
+                    decs.push(pos1);     
+                    editor.setDecorations(decorationType,decs);                  
                 }
                 i+=1;
             });
